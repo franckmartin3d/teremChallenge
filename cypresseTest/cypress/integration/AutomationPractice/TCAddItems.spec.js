93b0ab42243e92a9c880814119b36db2006c1500
Francois Martin <franck.martin3d@gmail.com>
2019-11-21 20:01:16 +1100

    added TC Blueprint Additems & Wishlist Quantities

---
+++ cypresseTest/cypress/integration/AutomationPractice/TCAddItems.spec.js
@@ -0,0 +1,28 @@
+describe("Test Case Add Items", () => {
+    // Navigating to myWishlist Page
+    it("Add Items to whistlist", () => {
+
+    //PREREQUIST
+    // navigate to website
+      cy.visit(
+        "http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist"
+      );
+      
+
+    //Log in user
+
+    //Create a whishlist
+
+    //STEPS TO TEST
+    // 1. In Popular Item select "Faded Short Sleeve T-shirts
+    // 2. On the right side of the screen select "Add to wishlist"
+    // 3. Navigate to your wishlist (user => MY WHISHLIST)
+    // 4. Select the wishlist in the table
+    // 5. Sign out
+    // 6. Sign In
+    // 7 Select My wishlist
+    // 8. Select the created wishlist
+    });
+
+});
+
---
+++ cypresseTest/cypress/integration/AutomationPractice/TCWishlistQuantities.spec copy.js
@@ -0,0 +1,42 @@
+describe("Test Case Add Items", () => {
+    // Navigating to myWishlist Page
+    it("Add Items to whistlist", () => {
+
+    //PREREQUIST
+    // navigate to website
+      cy.visit(
+        "http://automationpractice.com/index.php?fc=module&module=blockwishlist&controller=mywishlist"
+      );
+      
+
+    //Log in user
+
+    //Create a whishlist
+
+    //STEPS TO TEST
+    // 1. In Popular Item select "Faded Short Sleeve T-shirts
+    // 2. On the right side of the screen select "Add to wishlist"
+    // 3. Navigate to your wishlist (user => MY WHISHLIST)
+    
+    // 4. In Popular Item select "Faded Short Sleeve T-shirts
+    // 5. On the right side of the screen select "Add to wishlist"
+    // 6. Navigate to your wishlist (user => MY WHISHLIST)
+    
+    // 7. In Popular Item select "Faded Short Sleeve T-shirts
+    // 8. Select Size M
+    // 9. On the right side of the screen select "Add to wishlist"
+    // 10. Navigate to your wishlist (user => MY WHISHLIST)
+    
+    // 11. In Popular Item select "Faded Short Sleeve T-shirts
+    // 12. Select Size L
+    // 13. On the right side of the screen select "Add to wishlist"
+    // 14. Navigate to your wishlist (user => MY WHISHLIST)
+    
+    // 15. In Popular Item select "Faded Short Sleeve T-shirts
+    // 16. Select color Blue
+    // 17. On the right side of the screen select "Add to wishlist"
+    // 18. Navigate to your wishlist (user => MY WHISHLIST)
+    });
+
+});
+
